089feec3480f64ac5fa0da462dceb14c8275675b
Qwertykitten <146257477+MaisieDeGraaf@users.noreply.github.com>
2024-02-25 14:53:45 -0500

    Map Updates

--- static/js/logic.js
+++ static/js/logic.js
@@ -6,7 +6,35 @@ d3.json(url).then(function(data){
     let markerGroup = L.geoJSON(data)
 
     console.log(markerGroup)
+
+    let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    })
     
+    let topo = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    });
+
+    let baseMaps = {
+        "Street Map": street,
+        "Topographic Map": topo
+    };
+
+    let overlayMaps = {
+        earthquakes: markerGroup
+    };
+
+    let myMap = L.map("map", {
+        center: [
+            37.09, -95.71
+        ],
+        zoom: 5,
+        layers: [street, markerGroup]
+    });
+
+    L.control.layers(baseMaps, overlayMaps, {
+        collapsed: false
+    }).addTo(myMap);
 
 })
 
