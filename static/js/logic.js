98253237e59835b38753985c980b8e35a215c34b
Qwertykitten <146257477+MaisieDeGraaf@users.noreply.github.com>
2024-02-25 17:53:28 -0500

    Finished Bonus

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,89 +1,109 @@
+//urls for data
 let url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson';
+let plateurl = 'static/js/PB2002_boundaries.json';
 
+//colour changes for depth of the earthquakes
 function getColour(depth) {
     if (depth > 89) return 'red';
-    if (depth > 69) return  'orange';
-    if (depth > 49) return  'yellow';
-    if (depth > 29) return  'green';
-    if (depth > 9) return  'lime';
-    else return 'black';
+    if (depth > 69) return 'orangered';
+    if (depth > 49) return 'orange';
+    if (depth > 29) return 'yellow';
+    if (depth > 9) return 'limegreen';
+    else return 'green';
+}
+//combine both plate and earthquake data and promise
+Promise.all([
+    d3.json(url),
+    d3.json(plateurl)
+]).then(function([earthquakeData, plateData]) {
+    console.log(earthquakeData);
+    console.log(plateData);
 
-};
-
-d3.json(url).then(function(data) {
-    console.log(data);
-
-    let markerGroup = L.geoJSON(data, {
+    //design markers and popups for markers
+    let markerGroup = L.geoJSON(earthquakeData, {
         pointToLayer: function(feature, latlng) {
             let markerSize = feature.properties.mag * 5;
+            let markerColour = feature.geometry.coordinates[2]; //to get the depth, is item 3
+            let popupContent = `
+            <b>Magnitude:</b> ${feature.properties.mag}<br>
+            <b>Place:</b> ${feature.properties.place}<br>
+            <b>Tsunami:</b> ${feature.properties.tsunami ? "Yes" : "No"}<br>
+            <b>ID:</b> ${feature.id}<br>
+            <b>Time:</b> ${new Date(feature.properties.time).toLocaleString()}
+        `;
+        let marker = L.circleMarker(latlng, {
+            radius: markerSize,
+            fillColor: getColour(markerColour),
+            color: 'black',
+            weight: 2,
+            opacity: 0.5,
+            fillOpacity: 0.9
+        }).bindPopup(popupContent);
 
-            let markerColor = feature.geometry.coordinates[2]; 
-            return L.circleMarker(latlng, {
-                radius: markerSize,
-                fillColor: getColour(markerColor),
-                color: 'black',
-                weight: 2,
-                opacity: 0.5,
-                fillOpacity: 0.6
-            });
+            return marker;
         }
     });
 
-
-
-    let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    //start to create and layer map and tiles
+    let plateLayer = L.geoJSON(plateData);
+        
+    let sattelite = L.tileLayer('https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryTopo/MapServer/tile/{z}/{y}/{x}', {
+        maxZoom: 20,
+        attribution: 'Tiles courtesy of the <a href="https://usgs.gov/">U.S. Geological Survey</a>'
     });
 
     let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     });
 
+    let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    });
+
     let baseMaps = {
+        "Satellite": sattelite,
         "Topographic Map": topo,
         "Street Map": street
     };
 
     let overlayMaps = {
+        "Tectonic Plates": plateLayer,
         "Earthquakes": markerGroup
     };
 
     let myMap = L.map("map", {
         center: [37.09, -95.71],
-        zoom: 5,
-        layers: [street, markerGroup]
+        zoom: 3,
+        layers: [sattelite, markerGroup]
     });
 
+    //add controls to the map
     L.control.layers(baseMaps, overlayMaps, {
         collapsed: false
     }).addTo(myMap);
 
-    const colors = {
+    //colour displays for the legend
+    const colours = {
         'red': '90+',
-        'lightred': '70 - 89',
+        'orangered': '70 - 89',
         'orange': '50 - 69',
-        'khaki': '30 - 49',
+        'yellow': '30 - 49',
         'limegreen': '10 - 29',
         'green': '-10 - 10'
     };
     
-    
+    //add legend to the map
     let legend = L.control({ position: 'bottomright' });
-
     legend.onAdd = function(map) {
         let div = L.DomUtil.create('div', 'info legend');
         let labels = [];
-    
-        for (const color in colors) {
+        for (const colour in colours) {
             labels.push(
-                '<i style="background:' + color + '"></i> ' +
-                colors[color]);
+                '<i style="background:' + colour + '"></i> ' +
+                colours[colour]);
         }
-    
         div.innerHTML = labels.join('<br>');
         return div;
     };
-    
     legend.addTo(myMap);
 });
-
