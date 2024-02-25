68d88921fc5c5a6c3a4cca78e189ff7f932dd4b5
Qwertykitten <146257477+MaisieDeGraaf@users.noreply.github.com>
2024-02-25 15:46:57 -0500

    Adding markers

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,33 +1,55 @@
-let url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson'
+let url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson';
 
-d3.json(url).then(function(data){
-    console.log(data)
+function getColour(depth) {
+    if (depth > 89) return 'red';
+    if (depth > 69) return  'orange';
+    if (depth > 49) return  'yellow';
+    if (depth > 29) return  'green';
+    if (depth > 9) return  'lime';
+    else return 'black';
 
-    let markerGroup = L.geoJSON(data)
+};
 
-    console.log(markerGroup)
+d3.json(url).then(function(data) {
+    console.log(data);
+
+    let markerGroup = L.geoJSON(data, {
+        pointToLayer: function(feature, latlng) {
+            let markerSize = feature.properties.mag * 5;
+
+            let markerColor = feature.geometry.coordinates[2]; 
+            return L.circleMarker(latlng, {
+                radius: markerSize,
+                fillColor: getColour(markerColor),
+                color: 'black',
+                weight: 2,
+                opacity: 0.5,
+                fillOpacity: 0.6
+            });
+        }
+    });
+
+
 
     let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    })
-    
-    let topo = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    });
+
+    let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     });
 
     let baseMaps = {
-        "Street Map": street,
-        "Topographic Map": topo
+        "Topographic Map": topo,
+        "Street Map": street
     };
 
     let overlayMaps = {
-        earthquakes: markerGroup
+        "Earthquakes": markerGroup
     };
 
     let myMap = L.map("map", {
-        center: [
-            37.09, -95.71
-        ],
+        center: [37.09, -95.71],
         zoom: 5,
         layers: [street, markerGroup]
     });
@@ -36,5 +58,32 @@ d3.json(url).then(function(data){
         collapsed: false
     }).addTo(myMap);
 
-})
+    const colors = {
+        'red': '90+',
+        'lightred': '70 - 89',
+        'orange': '50 - 69',
+        'khaki': '30 - 49',
+        'limegreen': '10 - 29',
+        'green': '-10 - 10'
+    };
+    
+    
+    let legend = L.control({ position: 'bottomright' });
+
+    legend.onAdd = function(map) {
+        let div = L.DomUtil.create('div', 'info legend');
+        let labels = [];
+    
+        for (const color in colors) {
+            labels.push(
+                '<i style="background:' + color + '"></i> ' +
+                colors[color]);
+        }
+    
+        div.innerHTML = labels.join('<br>');
+        return div;
+    };
+    
+    legend.addTo(myMap);
+});
 
